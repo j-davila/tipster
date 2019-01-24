977a047e77ad7e78b0d51c8db0c5f48a69fee15c
Jose <josedavila@joses-mac.local>
2019-01-23 18:21:49 -0800

    Initial Commit

---
+++ tipster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipster
+//
+//  Created by Jose  Davila on 1/23/19.
+//  Copyright © 2019 Jose  Davila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
